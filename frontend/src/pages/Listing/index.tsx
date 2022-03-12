8da04e714c2f8d3d02f5920c0907648a7436ec1f
Evandro Domingues <evsdomingues@gmail.com>
2022-03-12 19:42:39 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
-  import MovieCard from "components/MovieCard";
+import axios from "axios";
+import MovieCard from "components/MovieCard";
 import Pagination from "Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
+
     return (
         <>
             <Pagination />
